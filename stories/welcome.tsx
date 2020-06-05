ca9ca8735da4918525d4661da4e014cafcc6e15a
Kevin Haube <thekevinhaube@gmail.com>
2020-06-05 19:36:23 -0400

    docs(storybook): welcome page

--- stories/welcome.tsx
+++ stories/welcome.tsx
@@ -1,6 +1,14 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import { Typography } from "../src";
 
 storiesOf("Welcome", module).add("Introduction", () => (
-  <h1>Welcome to HospitalRun Storybook</h1>
+  <div style={{ padding: "2rem" }}>
+    <Typography styleAs="display2">Component Library</Typography>
+    <div>
+      <Typography variant="h1" styleAs="h4">
+        A bespoke component library by Kevin Haube
+      </Typography>
+    </div>
+  </div>
 ));
