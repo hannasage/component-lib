830e0f0452b9a1ca67fbb63e2de9f2d1566dd88e
Kevin Haube <thekevinhaube@gmail.com>
2020-06-10 14:18:58 -0400

    docs(surface): examples for Storybook
    Added an examples section showcasing the versatility of the Surface component.
    
    Issue #2

--- stories/surface.stories.tsx
+++ stories/surface.stories.tsx
@@ -1,7 +1,66 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import { useMediaQuery } from "../src/hooks";
 
-import { Surface } from "../src";
+import { Surface, Typography, Button } from "../src";
+
+const ExampleOne = () => {
+  const isSmall = useMediaQuery("(max-width: 640px");
+  const isMedium = useMediaQuery("(max-width: 840px");
+
+  return (
+    <Surface
+      size={isSmall || isMedium ? ["100%", "auto"] : ["33%", "auto"]}
+      style={{ maxWidth: "700px", padding: "1rem" }}
+    >
+      <Typography variant="h3">Title</Typography>
+      <Typography variant="h5">Subtitle</Typography>
+      <Typography style={{ marginTop: "1rem" }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget
+        felis leo. Donec sit amet finibus lorem, sit amet vehicula quam.
+        Maecenas nec justo dictum, lobortis tortor in, vulputate nulla. Aenean
+        urna tortor, efficitur posuere ultrices ut, auctor ut odio. Phasellus
+        ultricies ac magna in bibendum. Ut vitae magna vitae nibh varius
+        imperdiet.
+      </Typography>
+    </Surface>
+  );
+};
+
+const ExampleTwo = () => {
+  const isSmall = useMediaQuery("(max-width: 640px");
+  const isMedium = useMediaQuery("(max-width: 840px");
+
+  return (
+    <Surface
+      size={isSmall || isMedium ? ["100%", "auto"] : ["33%", "auto"]}
+      style={{
+        color: "white",
+        maxWidth: "700px",
+        padding: "1rem",
+        backgroundColor: "#1EA7FD",
+        boxShadow: "-4px 4px 10px 4px rgba(0,0,0,0.2)",
+      }}
+    >
+      <Typography variant="h3">Alert</Typography>
+      <Typography style={{ marginTop: "1rem" }}>
+        You have created an example component on top of the Surface component
+        and API. You have also added a button to the surface.
+      </Typography>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginTop: "1rem",
+        }}
+      >
+        <Button style={{ margin: 0 }} color="#f7f7f7" textColor="#1EA7FD">
+          Confirm
+        </Button>
+      </div>
+    </Surface>
+  );
+};
 
 storiesOf("Surface", module)
   .addParameters({
@@ -34,7 +93,10 @@ storiesOf("Surface", module)
         flexDirection: "row",
       }}
     >
-      <Surface className="surface-test-class"></Surface>
+      <Surface
+        className="surface-test-class"
+        size={["200px", "200px"]}
+      ></Surface>
       <Surface
         style={{
           backgroundColor: "#1EA7FD",
@@ -60,5 +122,20 @@ storiesOf("Surface", module)
         size={["100%", "16rem"]}
         style={{ maxWidth: "200px" }}
       ></Surface>
+    </div>
+  ))
+  .add("Examples", () => (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+      }}
+    >
+      <ExampleOne />
+      <ExampleTwo />
     </div>
   ));
