b88bbfc6a03d965956feacb5f64a923fa93ef8e7
Kevin Haube <thekevinhaube@gmail.com>
2020-06-12 15:40:13 -0400

    test(surface): surface testing
    Issue #12

---
+++ test/surface.test.tsx
@@ -0,0 +1,18 @@
+import { shallow } from "enzyme";
+import * as React from "react";
+
+import { Surface } from "../src";
+
+describe("Surface", () => {
+  it("Renders without crashing", () => {
+    const wrapper = shallow(<Surface />);
+    const element = wrapper.find(HTMLDivElement);
+    expect(element).toHaveLength(1);
+  });
+
+  it("Applies Dark Mode default styling through darkMode property", () => {
+    const wrapper = shallow(<Surface darkMode={true} />);
+    const element = wrapper.find(HTMLDivElement);
+    expect(element.prop("darkMode")).toBe(true);
+  });
+});
